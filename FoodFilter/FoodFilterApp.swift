b5a5a03a4152d97335dd83c7442a78c6786e58e7
nadolf <96559304+nadolf@users.noreply.github.com>
2024-12-19 11:13:30 -0500

    Initial Commit

---
+++ FoodFilter/FoodFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFilterApp.swift
+//  FoodFilter
+//
+//  Created by Nadolf Tagoe on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
